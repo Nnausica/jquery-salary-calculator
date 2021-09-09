30a8c49165788642457bd93c75ecb7e17ba37586
Laura Burbank <laura.burbank@gmail.com>
2021-09-09 10:46:05 -0500

    salaries go into an array now, still strings

--- script/client.js
+++ script/client.js
@@ -31,36 +31,35 @@ function addEmployee(){
     employees.push(newEmployee); // adds newly entered employee to the array
 };
 
-function deleteEmployee(){
-    //on delete button click
-    // $('#someotherexsistingbuttonclick').on('click', "deleteEmployeeInfo", deleteEmployee) THIS NEEDS TO GO SOMEWHERE?
-      //link to other button?
-    $(this).parent().remove()
-    // remove the whole row of the table. 
-  
-}
+payroll=[];
 
 function addSalary(){
-    let total=0
+    payroll.push(employees[employees.length-1].salary)
 
-    for (i=0; i<employees.length; i++){
-        total += employees[i].salary
-    };
-    console.log(`total salary?`, total);
+    total = 0
 
-    $(`#calculation`).empty().val(``);
-    $(`#calculation`).append(`Salary =`+ total);
+    for(i=0; i<payroll.length; i++){
+        total += payroll[i]
+    }
+
+    $(`#calculation`).empty().val(``); //emptys previous salary number 
+    $(`#calculation`).append(`Salary =`+ total); //appends with new  number
+
+    //if (total > 20,000){$(`#.red`)}
 }; //end addSalary
-
-    // need to emtpy out content in #calculation
-    // then append with new sum on submit button click
-
+////// THIS IS ALL STRINGS, HOW DO I MAKE IT NUMBERS????/////
 
 
 //store the information to calculate monthly costs, 
-
 //Using the stored information, calculate monthly costs and append this to the to DOM. 
-
 //If the total monthly cost exceeds $20,000, add a red background color to the total monthly cost.
 
+
 // Create a delete button that removes an employee from the DOM. For Base mode, it does **not** need to remove that Employee's salary from the reported total.
+function deleteEmployee(){
+    //on delete button click
+    $(`#someotherexistingbutton`).on('click', "deleteEmployeeInfo", deleteEmployee) //THIS NEEDS TO GO SOMEWHERE?
+      //link to other button?
+    $(this).parent().remove()
+    // remove the whole row of the table. 
+}
